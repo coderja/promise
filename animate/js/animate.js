c5e83592c42284bd5d449eeb27599a86f746f5a7
jeiiy.wang <jeiiy.wang@wingconn.com>
2018-08-18 17:53:53 +0800

    闰年判断

--- animate/js/animate.js
+++ animate/js/animate.js
@@ -17,12 +17,8 @@
     * 当270<deg<=360,deg=360-(c||d);
     10
 * */ 
-<<<<<<< HEAD
 
-function getmatrix(a,b,c,d) { 
-=======
 function getmatrix(a,b,c,d){ 
->>>>>>> 3468f8aca62fad546f22fcf68a1b4d0c12310c17
     var aa=Math.round(180*Math.asin(a)/ Math.PI); 
     var bb=Math.round(180*Math.acos(b)/ Math.PI); 
     var cc=Math.round(180*Math.asin(c)/ Math.PI); 
@@ -39,27 +35,27 @@ function getmatrix(a,b,c,d){
     //return (aa+','+bb+','+cc+','+dd); 
 }
 
-<<<<<<< HEAD
 $(()=>{
-    var currentYear=2004,currentMonth=04,currentDate=01;
+    var currentYear=2004,currentMonth=04,currentDate=01,currentYearDeg=0,currentMonthDeg=0,currentDateDeg=0;
     // 旋转方法
     function rotate(elem,$target) {
         var $elem = $(elem);
         if($target.is(elem+' span')) {
-            var nowDeg = String($target.css('transform'))
+             nowDeg = String($target.css('transform'))
             var values = nowDeg.split('(')[1].split(')')[0].split(',');
             var a = values[0];
             var b = values[1];
             var c = values[2];
             var d = values[3];
             nowDeg = getmatrix(a,b,c,d);
+            
             $(elem+' .active').removeClass('active')
             if(elem=='#date'){
                 currentDate = $target.html()
             }else if(elem=='#year'){
                 currentYear = $target.html()
             }else{
-                currentMonth = parseInt($target.html())+1
+                currentMonth = parseInt($target.html())
             }
             
             if(nowDeg>0&&nowDeg<=180) {
@@ -73,7 +69,6 @@ $(()=>{
                 $elem.css({'transform':'rotate('+(0)+'deg)'});
             }
             $target.addClass('active')
-            console.log(nowDeg)
         }
     }
 
@@ -86,7 +81,7 @@ $(()=>{
     //旋转月份
     $('#month').on('click',function(e) {
         var $target = $(e.target)
-        console.log(currentMonth)
+        rotate('#month',$target)
         switch(currentMonth){
             case 1:
             createDate(31);
@@ -128,7 +123,12 @@ $(()=>{
             createDate(30);
 
         }
-        rotate('#month',$target)
+        // 闰年计算公式
+        if((currentYear%4)==0&&(currentYear%100)!=0||currentYear%400==0){
+            if(currentMonth==2){
+                createDate(29)
+            }
+        }
     })
 
     // 生成日期
@@ -168,76 +168,6 @@ $(()=>{
         $('#date').addClass('date_anima')
         $('.ok').addClass('ok_anima')
     })
-=======
-// 旋转方法
-function rotate(elem,$target){
-    var $elem = $(elem);
-    if($target.is(elem+' span')){
     
-        var nowDeg = String($target.css('transform'))
-        var values = nowDeg.split('(')[1].split(')')[0].split(',');
-        var a = values[0];
-        var b = values[1];
-        var c = values[2];
-        var d = values[3];
-
-        nowDeg = getmatrix(a,b,c,d);
-        $('.active').removeClass('active')
-        var $val = $target.val()
-        
-        var prevMonth = 0;
-
-        if(nowDeg>0&&nowDeg<=180){
-            $elem.css({'transform':'rotate('+(0-nowDeg)+'deg)'});
-        }else if(nowDeg>270){
-            $elem.css({'transform':'rotate('+(360-nowDeg)+'deg)'});
-        }else if(nowDeg>180&&nowDeg<=270){
-            $elem.css({'transform':'rotate('+(0-nowDeg)+'deg)'});
-        }else{
-            $elem.css({'transform':'rotate('+(0)+'deg)'});
-        }
-         $target.addClass('active')
-    }
-}
-
-
-
-
-//旋转年份
-$('#year').on('click',function(e){
-    var $target = $(e.target)
-    rotate('#year',$target)
 })
 
-//旋转月份
-$('#month').on('click',function(e){
-    var $target = $(e.target)
-    rotate('#month',$target)
-})
-
-//旋转日期
-$('#date').on('click',function(e){
-    var $target = $(e.target)
-    rotate('#date',$target)
-})
-
-// 点击OK事件
-$('.ok').on('click',function(e){
-    $('.box').addClass('hide')
-    $('.ball').addClass('ball_animation').show()
-    $('#year').removeClass('year_anima')
-    $('#month').removeClass('month_anima')
-    $('#date').removeClass('date_anima')
-    $(this).removeClass('ok_anima')
-})
-
-// 点击小球事件
-$('.ball').on('click',function(e){
-    $('.box').removeClass('hide')
-    $(this).removeClass('ball_animation').hide();
-    $('#year').addClass('year_anima')
-    $('#month').addClass('month_anima')
-    $('#date').addClass('date_anima')
-    $('.ok').addClass('ok_anima')
->>>>>>> 3468f8aca62fad546f22fcf68a1b4d0c12310c17
-})
